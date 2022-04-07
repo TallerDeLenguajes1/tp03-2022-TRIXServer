45eee3644a877da3b0f9837a884b183c5d303569
TRIXServer <admin@trixserver.com>
2022-04-07 18:26:04 -0300

    commit

--- tp03-pt2.c
+++ tp03-pt2.c
@@ -4,14 +4,20 @@
 
 #define MAX 50
 
-char nombre[MAX];
-
-int *pNombre[5];
-
+char *nombre;
+char *buff;
 
 int main(int argc, char const *argv[])
-{
+{    
+    buff = (char *) malloc(MAX * sizeof(char));
     
+    printf("Ingrese un nombre: ");
+    gets(buff);
+
+    puts(buff);
+
+    free(buff);
+
     return 0;
 }
 
