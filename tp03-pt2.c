a1ea104772a12f681540d10f422b0c30d6145562
TRIXServer <admin@trixserver.com>
2022-04-07 17:54:17 -0300

    commit

---
+++ tp03-pt2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 50
+
+char nombre[MAX];
+
+int *pNombre[5];
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
